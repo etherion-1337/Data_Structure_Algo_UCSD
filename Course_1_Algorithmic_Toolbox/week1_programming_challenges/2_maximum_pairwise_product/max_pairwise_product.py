b598cca9fc66114ead955d14a49db6bc064fa5e3
etherion <46531622+etherion-1337@users.noreply.github.com>
2020-05-08 17:18:44 +0800

    Update max_pairwise_product.py

--- Course_1_Algorithmic_Toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
+++ Course_1_Algorithmic_Toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
@@ -10,8 +10,26 @@ def max_pairwise_product(numbers):
 
     return max_product
 
+def max_pairwise_product_sort(numbers):
+    """Sort the list first, then take the last two number (biggest) and multiply them
+    """
+    sorted_list = sorted(numbers)
+    ans = sorted_list[-1]*sorted_list[-2]
+    return ans
+
+def max_pairwise_product_fast(numbers):
+    """Find the largest 2 numbers by scanning through the list
+    """
+    num_list = numbers.copy()
+    max_num_1 = max(num_list)
+    num_list.remove(max_num_1)
+    max_num_2 = max(num_list)
+    ans = max_num_1*max_num_2
+    return ans
+
 
 if __name__ == '__main__':
     input_n = int(input())
     input_numbers = [int(x) for x in input().split()]
-    print(max_pairwise_product(input_numbers))
+    print(max_pairwise_product_sort(input_numbers))
+
