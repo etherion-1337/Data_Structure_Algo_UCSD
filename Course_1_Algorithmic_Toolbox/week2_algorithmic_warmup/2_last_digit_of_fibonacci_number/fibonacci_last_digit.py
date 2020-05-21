adf8895cf5015866803ec12823a4a644431c34f4
etherion <46531622+etherion-1337@users.noreply.github.com>
2020-05-22 00:34:51 +0800

    week 2 update

--- Course_1_Algorithmic_Toolbox/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
+++ Course_1_Algorithmic_Toolbox/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
@@ -13,6 +13,18 @@ def get_fibonacci_last_digit_naive(n):
 
     return current % 10
 
+def calc_fib_fast(n):
+    """ Directly use maths formula
+    """
+    if n <= 1:
+        return n
+    else:
+        fib_p = ((1+5**0.5)/2)**n
+        fib_m = ((1-5**0.5)/2)**n
+        fib = (fib_p - fib_m)/(5**0.5)
+        return int(fib)
+
+
 if __name__ == '__main__':
     input = sys.stdin.read()
     n = int(input)
