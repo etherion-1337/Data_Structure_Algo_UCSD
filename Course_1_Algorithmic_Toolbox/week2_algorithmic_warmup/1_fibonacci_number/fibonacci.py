248e96dc76f23ff54febdd2d4723203a3790bef5
etherion <46531622+etherion-1337@users.noreply.github.com>
2020-05-22 00:17:33 +0800

    fib updates

--- Course_1_Algorithmic_Toolbox/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ Course_1_Algorithmic_Toolbox/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -5,5 +5,15 @@ def calc_fib(n):
 
     return calc_fib(n - 1) + calc_fib(n - 2)
 
+def calc_fib_fast(n):
+    if n <= 1:
+        return n
+    else:
+        fib_p = ((1+5**0.5)/2)**n
+        fib_m = ((1-5**0.5)/2)**n
+        fib = (fib_p - fib_m)/(5**0.5)
+        return fib
+
 n = int(input())
 print(calc_fib(n))
+print(calc_fib_fast(n))
