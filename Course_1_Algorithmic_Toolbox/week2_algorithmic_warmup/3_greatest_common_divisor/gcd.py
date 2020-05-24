324913aa6687872c249b71738ec0ea8c5041b6d3
etherion <46531622+etherion-1337@users.noreply.github.com>
2020-05-24 18:59:11 +0800

    week 3 assignment 3

--- Course_1_Algorithmic_Toolbox/week2_algorithmic_warmup/3_greatest_common_divisor/gcd.py
+++ Course_1_Algorithmic_Toolbox/week2_algorithmic_warmup/3_greatest_common_divisor/gcd.py
@@ -2,6 +2,8 @@
 import sys
 
 def gcd_naive(a, b):
+    """brutal way to get gcd
+    """
     current_gcd = 1
     for d in range(2, min(a, b) + 1):
         if a % d == 0 and b % d == 0:
@@ -10,7 +12,20 @@ def gcd_naive(a, b):
 
     return current_gcd
 
+def gcd_algo(a,b):
+    """find gcd using Euclidean algo
+    """
+    i = max(a,b)
+    j = min(a,b)
+
+    if j == 0:
+        return i
+    else:
+        reminder = i%j
+        return gcd_algo(j, reminder)
+
 if __name__ == "__main__":
     input = sys.stdin.read()
     a, b = map(int, input.split())
-    print(gcd_naive(a, b))
+    # print(gcd_naive(a, b))
+    print(gcd_algo(a,b))
