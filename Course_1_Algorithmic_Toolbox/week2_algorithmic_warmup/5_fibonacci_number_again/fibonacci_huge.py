d99ccbadfd5b7c6cb511a68692ebfad8ebc240f3
etherion <46531622+etherion-1337@users.noreply.github.com>
2020-05-25 00:43:59 +0800

    week 2 continue

--- Course_1_Algorithmic_Toolbox/week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.py
+++ Course_1_Algorithmic_Toolbox/week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.py
@@ -13,7 +13,29 @@ def get_fibonacci_huge_naive(n, m):
 
     return current % m
 
+def get_fibonacci_huge(n,m):
+    previous = 0
+    current = 1
+    
+    for i in range(m*m):
+        previous, current = current, (current+previous)%m
+        if (previous, current) == (0,1):
+            period = i + 1
+            break
+
+    j = n%period
+
+    if j == 0:
+        return j
+    else:
+        previous = 0
+        current = 1
+        for i in range (2, j+1):
+            previous, current = current, (current+previous)%m
+        return current
+
 if __name__ == '__main__':
     input = sys.stdin.read();
     n, m = map(int, input.split())
-    print(get_fibonacci_huge_naive(n, m))
+    # print(get_fibonacci_huge_naive(n, m))
+    print(get_fibonacci_huge(n,m))
--- Course_1_Algorithmic_Toolbox/week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.py
+++ Course_1_Algorithmic_Toolbox/week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.py
@@ -15,7 +15,30 @@ def fibonacci_sum_naive(n):
 
     return sum % 10
 
+
+def get_fibonacci_huge(n,m):
+    previous = 0
+    current = 1
+    
+    for i in range(m*m):
+        previous, current = current, (current+previous)%m
+        if (previous, current) == (0,1):
+            period = i + 1
+            break
+
+    j = n%period
+
+    if j == 0:
+        return j
+    else:
+        previous = 0
+        current = 1
+        for i in range (2, j+1):
+            previous, current = current, (current+previous)%m
+        return current
+
 if __name__ == '__main__':
     input = sys.stdin.read()
     n = int(input)
-    print(fibonacci_sum_naive(n))
+    # print(fibonacci_sum_naive(n))
+    print((get_fibonacci_huge(n+2,10)-1)%10)
